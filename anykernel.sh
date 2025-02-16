969498859772a3fc9e5752c222f78179293f73ef
YuzakiKokuban <heibanbaize@gmail.com>
2025-02-16 15:57:06 +0800

    修改：     anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -5,13 +5,13 @@
 # global properties
 properties() { '
 kernel.string=Z Fold/Flip 4-Knox-Disabled-Kernel-Kokuban
-do.devicecheck=0
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
+device.name1=b4q
+device.name2=q4q
 device.name3=
 supported.versions=
 supported.patchlevels=
