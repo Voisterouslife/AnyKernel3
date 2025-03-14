a34dff2ba3453c679d9716413712d86dcf04c6df
YuzakiKokuban <heibanbaize@gmail.com>
2025-03-14 22:41:44 +0800

    修改：     anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Z Fold/Flip 4-Knox-Disabled-Kernel-Kokuban
-do.devicecheck=1
+kernel.string=TabS7-KernelSU-SukiSU-Kokuban
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=b4q
-device.name2=q4q
+device.name1=
+device.name2=
 device.name3=
 supported.versions=
 supported.patchlevels=
