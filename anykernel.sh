55dd6aea89ac7468d7dc8c1b2d3e7fb2364a6e5d
YuzakiKokuban <heibanbaize@gmail.com>
2025-05-19 22:40:04 +0800

    修改：     anykernel.sh 	删除：     patch_linux

--- anykernel.sh
+++ anykernel.sh
@@ -4,8 +4,8 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=S24/ZFold6/ZFlip6-KernelSU-AnyKernel3-Kokuban-Elysia
-do.devicecheck=1
+kernel.string=TabS10-KernelSU-AnyKernel3-Kokuban-Exusiai
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
