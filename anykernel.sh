3321cedac83c0da0b0a1a7f567d35bd1e2ef511f
YuzakiKokuban <heibanbaize@gmail.com>
2025-08-21 17:32:06 +0800

    修改内核名

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=S24/ZFold6/ZFlip6-KernelSU-AnyKernel3-Kokuban-Elysia
+kernel.string=S24-Snapdragon-Kokuban-Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
